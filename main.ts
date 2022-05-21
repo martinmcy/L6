b15646a3ff7769f5b12c736ae215d4aa86db4e27
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-05-21 10:54:50 +0800

    1

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+OLED.init(128, 64)
+basic.forever(function () {
+    OLED.writeStringNewLine("Light:" + pins.digitalReadPin(DigitalPin.P0))
+    if (pins.digitalReadPin(DigitalPin.P0) < 200) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
